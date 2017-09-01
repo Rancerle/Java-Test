dca34c85ee9d2f3aeb1a1a64eb022d704710a419
Reilly Q. Curran <reillycurran880@gmail.com>
2017-09-01 14:19:00 -0400

    fixed

--- testfile.java
+++ testfile.java
@@ -1,7 +1,7 @@
-class HelloWorld
+class testfile
 {
     public static void main(String[] args)
     {
-        System.out.println("Test_Passed"); 
+        System.out.println("Test_Passed");
     }
 }
